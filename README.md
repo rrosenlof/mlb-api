8b9d1264a432cf756b0446d69fa72e13622c3151
ross <rosenlof.ross@gmail.com>
2020-10-02 00:02:43 -0600

    switching querying method

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Flask API using Lahman's Baseball Database, 2019 SQLite version.
 
-# How to Run Locally
+## How to Run Locally
 
 1. Clone this repo navigate inside the project directory.
 2. Create a `virtualenv` with the following commands:
@@ -20,10 +20,27 @@ $ python3 app.py
 ```
 5. Navigate to http://localhost:5000/. Add parameters to that url based on the guide below...
 
-# How to use this API
+## How to use this API
 
 Here is a list of endpoints of the REST API:
 
-## Parks
+### Parks
 
-- `http://localhost:5000/parks`: Returns all MLB parks as a JSON object
+- `http://localhost:5000/parks`: Returns all MLB parks
+
+### Teams
+
+- `http://localhost:5000/teams`: Returns all MLB teams
+- `http://localhost:5000/team/<franchID>`: Returns all MLB teams for a given franchID
+
+## Other info
+
+Uses this style guide for JSON responses: https://github.com/omniti-labs/jsend
+
+### To Do:
+
+- [ ] Add more methods... (which ones?)
+- [ ] Add some error checking on the various methods
+- [ ] Secure against sql injections
+- [ ] Host this somewhere
+- [ ] Get permission to do this...?
--- app.py
+++ app.py
@@ -1,20 +1,58 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.sql import select, bindparam
 from json import dumps
 
 e = create_engine('sqlite:///lahmansbaseballdb.sqlite')
+metadata = MetaData()
+parks = Table('parks', metadata, autoload=True, autoload_with=e)
+teams = Table('teams', metadata, autoload=True, autoload_with=e)
+
 
 app = Flask(__name__)
 api = Api(app)
 
-class Parks_Meta(Resource):
+class All_Parks(Resource):
     def get(self):
         conn = e.connect()
-        query = conn.execute("select * from parks;")
-        return {'parks': [i for i in query.cursor.fetchall()]}
+        query = select([parks])
+        res = conn.execute(query)
+        headers = [x for x in res.keys()]
+        json_data = []
+        for r in res:
+            json_data.append(dict(zip(headers,r)))
+        json_data = { 'parks': json_data }
+        return {'status': "success", 'data': json_data}
 
-api.add_resource(Parks_Meta, '/parks')
+
+class All_Teams(Resource):
+    def get(self):
+        conn = e.connect()
+        query = select([teams])
+        res = conn.execute(query)
+        headers = [x for x in res.keys()]
+        json_data = []
+        for r in res:
+            json_data.append(dict(zip(headers,r)))
+        json_data = { 'teams': json_data }
+        return {'status': "success", 'data': json_data}
+
+class Teams_By_Name(Resource):
+    def get(self, franchID: str):
+        conn = e.connect()
+        query = teams.select(teams.c.franchID.ilike(bindparam('franchID')))
+        # https://docs.sqlalchemy.org/en/13/core/tutorial.html#bind-parameter-objects
+        res = conn.execute(query, franchID=franchID)
+        headers = [x for x in res.keys()]
+        json_data = []
+        for r in res:
+            json_data.append(dict(zip(headers,r)))
+        return {'teams': [json_data]}
+
+api.add_resource(All_Parks, '/parks')
+api.add_resource(All_Teams, '/teams')
+api.add_resource(Teams_By_Name, '/team/<string:franchID>')
 
 if __name__ == '__main__':
     app.run()
